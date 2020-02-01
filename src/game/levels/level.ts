aef6f45de319e2871cb115ed30fe492de3cd3b30
Kamil Krzysztof Kulach <kamilk1512@gmail.com>
2020-02-01 17:08:29 +0100

    Reseting the player

--- src/game/levels/level.ts
+++ src/game/levels/level.ts
@@ -1,10 +1,17 @@
 import { GameWorld, Vector2, Engine, Vector3, SimObject } from "scrapy-engine";
 import { Player } from "../entities/player";
 import { Tile } from "../entities/tile";
+import { entityPosToTilePos } from "@/utils/position";
 
 export abstract class Level extends GameWorld{
 	public abstract entry:Vector2;
 	public abstract exit:Vector2;
+
+	public constructor(engine:Engine){
+		super(engine);
+		this.player = new Player(this.engine);
+		this.addChild(this.player)
+	}
 
 	protected abstract buildLevel():void;
 	public player:Player;
@@ -18,21 +25,27 @@ export abstract class Level extends GameWorld{
 		this.addChild(tile);
 	}
 
-	private setPlayer():void{
-		this.player = new Player(this.engine);
+	private resetPlayer():void{
 		this.player.transform.position.x = this.entry.x * 8;
 		this.player.transform.position.y = this.entry.y * 8;
-		this.addChild(this.player);
 	}
 
 	public update(dt:number):void{
 		if (!this.ready){
 			this.buildLevel();
-			this.setPlayer();
+			this.resetPlayer();
 			this.ready = true;
 		}
 		
 		super.update(dt);
+
+		let playerPos = entityPosToTilePos(this.player.transform.position)
+		if (playerPos.x < 0 || playerPos.x >= 10){
+			this.resetPlayer();
+		}
+		if (playerPos.y < 0 || playerPos.y >= 6){
+			this.resetPlayer();
+		}
 
 		for (let tile of this.tiles){
 			let collision = this.player.hitbox.isTouching(tile.hitbox);
--- src/game/levels/level1.ts
+++ src/game/levels/level1.ts
@@ -8,6 +8,7 @@ export class Level1 extends Level {
 	public exit: Vector2 = new Vector2(9, 1);
 
 	public buildLevel(): void {
-		this.addTile(new TileBox(this.engine, 10, 1, 0, 0))
+		this.addTile(new TileBox(this.engine, 3, 1, 0, 0))
+		this.addTile(new TileBox(this.engine, 3, 1, 7, 0))
 	}
 }
