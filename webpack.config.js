2879f488cdbfb0dac047b993ff34cdde4d7aac51
Kamil Krzysztof Kulach <kamilk1512@gmail.com>
2020-02-01 17:33:32 +0100

    Fixed eslint

--- webpack.config.js
+++ webpack.config.js
@@ -2,43 +2,43 @@ const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 
 let config = {
-	entry:{
-		index:"./src/index.ts",
+	entry: {
+		index: "./src/index.ts",
 	},
 	devtool: "source-map",
-	module:{
-		rules:[
+	module: {
+		rules: [
 			{
-				test:/\.tsx?$/,
-				use:"ts-loader",
+				test: /\.tsx?$/,
+				use: "ts-loader",
 				exclude: /node_modules/
 			},
 		]
 	},
-	resolve:{
-		extensions:[".tsx", ".ts", ".js"],
-		alias:{
+	resolve: {
+		extensions: [".tsx", ".ts", ".js"],
+		alias: {
 			assets: path.resolve(__dirname, "src/assets"),
-			"@":path.resolve(__dirname, "src")
+			"@": path.resolve(__dirname, "src")
 		}
 	},
-	output:{
-		filename:"[name].js",
+	output: {
+		filename: "[name].js",
 		path: path.resolve(__dirname, "dist"),
 	},
-	devServer:{
-		contentBase:path.join(__dirname, "dist"),
-		compress:true,
-		port:8081,
-		writeToDisk:true
-	},	
-	plugins:[
+	devServer: {
+		contentBase: path.join(__dirname, "dist"),
+		compress: true,
+		port: 8081,
+		writeToDisk: true
+	},
+	plugins: [
 		new CopyPlugin([
-			{from:path.resolve(__dirname, "public"), to:path.resolve(__dirname, "dist")}
+			{from: path.resolve(__dirname, "public"), to: path.resolve(__dirname, "dist")}
 		])
 	]
-}
+};
 
 module.exports = (env, args)=>{
 	return config;
-}
+};
